5c07cd25e4027f9f2a83ca3b626d7304a82a4d38
Larry Chiem <clarry@pdx.edu>
2020-06-12 00:48:16 -0700

    Reversed.
    Signed-off-by: Larry Chiem <clarry@pdx.edu>

--- LL/linkedlist.py
+++ LL/linkedlist.py
@@ -5,39 +5,39 @@ class Node:
 
 class SLinkedList:
     def __init__(self):
-        self.headval = None
+        self.head = None
         
     def listprint(self):
-        printval = self.headval
+        printval = self.head
         while printval is not None:
             print(printval.val)
             printval = printval.next
     
     def AtBeginning(self, newdata):
         NewNode = Node(newdata)
-        NewNode.next = self.headval
-        self.headval = NewNode
+        NewNode.next = self.head
+        self.head = NewNode
 
     def AtEnd(self, newdata):
         NewNode = Node(newdata)
-        if self.headval is None:
-            self.headval = NewNode
+        if self.head is None:
+            self.head = NewNode
             return
-        last = self.headval
+        last = self.head
         while(last.next):
             last = last.next
         last.next = NewNode
 
     def RemoveNode(self, RemoveKey):
 
-        HeadVal = self.headval
+        HeadVal = self.head
 
         if (HeadVal == None):
             return
 
         # If RemoveKey is first
         if (HeadVal.val == RemoveKey):
-            self.headval = HeadVal.next
+            self.head = HeadVal.next
             HeadVal = None
             return
         while (HeadVal.val != RemoveKey):
@@ -48,21 +48,38 @@ class SLinkedList:
             HeadVal = None
             return
 
-    def _ReverseList(self, head):
-        if head is None or head.next is None:
-            return head
-        p = self.ReverseList(head.next)
-        head.next.next = head
-        head.next = None
-        print(p.headval)
-        return p
+    def reverseUtil(self, curr, prev):
+
+        # If last node mark it head
+        if curr.next is None :
+            self.head = curr
+
+            # Update next to prev node
+            curr.next = prev
+            return
+
+        # Save curr.next node for recursive call
+        next = curr.next
+
+        # And update next
+        curr.next = prev
+
+        self.reverseUtil(next, curr)
+
+
+    # This function mainly calls reverseUtil()
+    # with previous as None
+    def reverse(self):
+        if self.head is None:
+            return
+        self.reverseUtil(self.head, None)
 
 list1 = SLinkedList()
-list1.headval = Node("Mon")
+list1.head = Node("Mon")
 e2 = Node("Tue")
 e3 = Node("Wed")
 # Link first Node to second Node
-list1.headval.next = e2
+list1.head.next = e2
 
 e2.next = e3
 
@@ -70,4 +87,7 @@ list1.AtBeginning("Sun")
 list1.AtEnd("Thu")
 
 list1.RemoveNode("Thu")
+list1.listprint()
+
+list1.reverse()
 list1.listprint()

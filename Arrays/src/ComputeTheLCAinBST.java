dfb3cfa89e7b58e754ba494ecbedfd65d65ceeb5
Larry Chiem <clarry@pdx.edu>
2020-09-11 23:22:13 -0700

    forgot notes

--- Arrays/src/ComputeTheLCAinBST.java
+++ Arrays/src/ComputeTheLCAinBST.java
@@ -1,4 +1,18 @@
+/* 15.4 COMPUTE THE LCA IN A BST
+Since a BST is a specialized binary tree, the notion of lowest common ancestor, as
+expressed in Problem 10.4 on Page 157, holdsfor BSTs too.
+In general, computing the LCA of two nodesin a BST is no easier than computing
+theLCA in a binary tree,since structurally a binary tree can be viewed as a BST where
+all the keys are equal. However, when the keys are distinct, it is possible to improve
+on the LCA algorithmsfor binary trees.
+Design an algorithm that takes asinput a BST and two nodes, and returns the LCA
+of the two nodes. For example, for the BST in Figure 15.1 on Page 255, and nodes C
+and G, your algorithm should return B. Assume all keys are distinct. Nodes do not
+have references to their parents.
+Hint: Take advantage of the BST property.*/
 public class ComputeTheLCAinBST {
+    /* Since we descend one level with each iteration, the time complexity is0(h), where h
+is the height of the tree.*/
     public static TreeNode findLCA(TreeNode tree,
                                             TreeNode s,
                                             TreeNode b) {
